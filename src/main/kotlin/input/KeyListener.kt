61315911a71f97f87db04f7523470e25422bf997
Aleksandras Vasinas <aleksandrasvasinas@gmail.com>
2023-04-18 01:20:37 +0300

    feat: update color logic

--- src/main/kotlin/input/KeyListener.kt
+++ src/main/kotlin/input/KeyListener.kt
@@ -7,11 +7,10 @@ import java.awt.event.KeyListener
 class KeyListener(private val game: Game) : KeyListener {
     override fun keyPressed(event: KeyEvent) {
         when (event.keyCode) {
-            KeyEvent.VK_UP -> game.movePiece("up")
             KeyEvent.VK_DOWN -> game.movePiece("down")
             KeyEvent.VK_LEFT -> game.movePiece("left")
             KeyEvent.VK_RIGHT -> game.movePiece("right")
-            KeyEvent.VK_ENTER -> game.rotatePiece()
+            KeyEvent.VK_UP -> game.rotatePiece()
         }
     }
 
